d048df8d1dc0a625db7676f0b6a762b83604a8af
juliodev <juliocesardeveloper@gmail.com>
2020-08-22 16:01:55 -0500

    webpack

---
+++ babel/src/js/index.js
@@ -0,0 +1,13 @@
+import '../css/index.css';
+import search from './search.js'
+import render from './render'
+
+const id = prompt('quien es ese pokemon')
+
+search(id)
+  .then((data) => {
+    render(data)
+  })
+  .catch(() => {
+    console.log('No hubo pokemon')
+  })
---
+++ react/src/js/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      Made in React.js
+    </div>
+  )
+}
+
+export default App
